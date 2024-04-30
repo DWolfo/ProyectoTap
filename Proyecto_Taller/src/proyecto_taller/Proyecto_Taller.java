b9255ded35bba385a69b6347832f7203fdade13d
DWolfo <100615589+DWolfo@users.noreply.github.com>
2024-04-29 18:39:51 -0600

    Add files via upload

---
+++ Proyecto_Taller/src/proyecto_taller/Proyecto_Taller.java
@@ -0,0 +1,17 @@
+
+package proyecto_taller;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Proyecto_Taller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
